c0a51912b7717c63babed60e4d24c03233bd8d51
pmchanhpk <pmchanhpk@gmail.com>
2017-02-02 22:20:39 +0700

    Update Tron2Mang_2.c

--- Tron2Mang_2.c
+++ Tron2Mang_2.c
@@ -1,112 +1,109 @@
 #include<stdio.h>
 
-void nhapMang(int a[], int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-	printf("Nhap phan tu thu %d: ", i+1);
-	scanf("%d", &a[i]);	
-	}	
-}
-
-void xuatMang(int a[], int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-	printf("%d ", a[i]);
-	}
-}
-
-
-void delete_vt(int a[], int &n, int vt)
-{
-	if(vt >= 0 && vt < n)
-	{
-		for (int i=vt; i < n-1; i++)
-		a[i] = a[i + 1];
-		n--;
-	}
-}
-
-int delete_so_can_xoa(int a[], int &n, int so_can_xoa)
-{
-	int so_so_0 = 0;
-	for (int i=0; i < n; i++)
-	{
-		if (a[i] == so_can_xoa)
-		{
-			delete_vt(a, n, i);
-			i--;
-			so_so_0++;
-		}
-	}
-	return so_so_0;
-}
-
-void insert(int a[], int &do_dai, int so_so_0)
-{
-	for (int i = 0; i < so_so_0; i++)
-	{
-		a[do_dai] = 0;
-		do_dai ++;
-	}
-}
-
-void ghepMang(int A[], int ptA, int B[], int ptB, int C[])
-{
-	for (int i = 0; i < ptA; i++)
-	{
-		C[i] = A[i];
-	}
-
-	for (int i=ptA;i< ptA + ptB; i++)
-	{
-		C[i] = B[i - ptA];
-	}
-}
-
-void hoanVi(int &a, int &b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void sapXepTang(int a[], int n)
-{
-	for (int i = 0; i < n - 1 ; i++)
-	{
-		for (int j = i+1; j<n; j++)
-		{
-			if (a[i] > a[j])
-			hoanVi(a[i], a[j]);
-		}
-	}
-}
+void getArray(int arr[], int len);
+void delElement(int arr[], int &len, int location);
+int NumberNeedDel(int arr[], int len, int number_need_del);
+void joinArray(int arrA[], int lenA, int arrB[], int lenB, int arrC[]);
+void ascendingOrder(int arr[], int len);
+void insert(int arr[], int &len, int count_number_0);
+void printArray(int arr[], int len);
 
 int main()
 {
-	int A[100], B[100], C[200], so_pt_A, so_pt_B, so_pt_C, so_so_0_A, so_so_0_B ;
+	int arrA[100], arrB[100], arrC[200], lenA, lenB, lenC, location, count_number_0_arrC, lenC_drop0;
+	printf("Nhap so luong phan tu mang A: ");
+	scanf("%d", &lenA);
+	getArray(arrA, lenA);
 	
-	printf("Nhap so phan tu mang A: ");
-	scanf("%d",&so_pt_A);
-	nhapMang(A, so_pt_A);
+	printf("Nhap so luong phan tu mang B: ");
+	scanf("%d", &lenB);
+	getArray(arrB, lenB);	
 	
-	printf("Nhap so phan tu mang B: ");
-	scanf("%d", &so_pt_B);
-	nhapMang(B, so_pt_B);
-	
-	so_so_0_A = delete_so_can_xoa(A, so_pt_A, 0);
-	so_so_0_B =	delete_so_can_xoa(B, so_pt_B, 0);
-	
-	ghepMang(A, so_pt_A, B, so_pt_B, C);
-	
-	sapXepTang(C, so_pt_A + so_pt_B);
-	
-	so_pt_C = so_pt_A + so_pt_B;
-	
-	
-	insert(C, so_pt_C , so_so_0_A + so_so_0_B);
-	xuatMang(C, so_pt_C);
-	return 0;	
+	joinArray(arrA, lenA, arrB, lenB, arrC);	
+	lenC = lenA + lenB;
+	count_number_0_arrC = NumberNeedDel(arrC, lenC, 0);	
+	lenC_drop0 = lenC - count_number_0_arrC;
+	ascendingOrder(arrC, lenC_drop0);	
+	insert(arrC, lenC_drop0, count_number_0_arrC);
+
+	printArray(arrC, lenC);
+	return 0;
+}
+
+void getArray(int arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf("phan tu thu %d la: ", i+1);
+		scanf("%d", &arr[i]);
+	}
+}
+
+void delElement(int arr[], int &len, int location)
+{
+	if (location >= 0 && location < len)
+	{
+		for (int i = location; i < len - 1; i++)	
+			arr[i] = arr[i + 1];
+		len--;
+	}	
+}
+
+int NumberNeedDel(int arr[], int len, int number_need_del)
+{ 
+	int count_number_0 = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (arr[i] == number_need_del)
+		{
+			delElement(arr, len, i);
+			i--;
+			count_number_0++;
+		}
+	}
+	return count_number_0;
+}
+
+void joinArray(int arrA[], int lenA, int arrB[], int lenB, int arrC[])
+{
+	for (int i = 0; i < lenA; i++)
+	{
+		arrC[i] = arrA[i];
+	}
+
+	for (int i = lenA; i < lenA + lenB; i++)
+	{
+		arrC[i] = arrB[i - lenA];
+	}
+}
+
+void ascendingOrder(int arr[], int len)
+{
+	for (int i = 0; i <  len - 1; i++)
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+}
+
+void insert(int arr[], int &len, int count_number_0)
+{
+	for (int i = 0; i < count_number_0; i++)
+	{
+		arr[len] = 0;
+		len++;
+	}
+}
+
+void printArray(int arr[], int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf("%d ", arr[i]);		
+	}
 }
